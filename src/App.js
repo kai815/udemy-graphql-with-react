e4f244bc1633f793d4a637058780c43f748ae7cb
kai815 <h.kaizuka815@gmail.com>
2020-06-19 11:23:51 +0900

    upate with write query

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { SEARCH_REPOSITORIES, ADD_STAR, REMOVE_STAR } from "./graphql";
 const StarButton = (props) => {
   const { node, query, first, last, before, after } = props;
   const totalCount = node.stargazers.totalCount;
-  const viewerHasStared = node.viewerHasStarred;
+  const viewerHasStarred = node.viewerHasStarred;
   const starCount = totalCount === 1 ? "1 star" : `${totalCount}stars`;
   const StarStatus = ({ addOrRemoveStar }) => {
     return (
@@ -14,22 +14,37 @@ const StarButton = (props) => {
         onClick={() => {
           addOrRemoveStar({
             variables: { input: { starrableId: node.id } },
+            update: (store, { data: { addStar, removeStar } } ) => {
+              const { starrable } = addStar || removeStar
+              const data = store.readQuery({
+                query: SEARCH_REPOSITORIES,
+                variables: { query, first, last, before, after }
+              });
+              const edges = data.search.edges
+              const newEdges = edges.map(edge => {
+                if (edge.node.id === node.id) {
+                  const totalCount = edge.node.stargazers.totalCount;
+                  // const diff = viewerHasStarred ? -1 : 1;
+                  const diff = starrable.viewerHasStarred ? 1 : -1;
+                  const newTotalCount = totalCount + diff;
+                  edge.node.stargazers.totalCount = newTotalCount;
+                }
+                return edge;
+              })
+              data.search.edges = newEdges;
+              store.writeQuery({ query: SEARCH_REPOSITORIES, data });
+            }
           });
         }}
       >
-        {starCount} | {viewerHasStared ? "starred" : "-"}
+        {starCount} | {viewerHasStarred ? "starred" : "-"}
       </button>
     );
   };
   return (
     <Mutation
-      mutation={viewerHasStared ? REMOVE_STAR : ADD_STAR}
-      refetchQueries={[
-        {
-          query: SEARCH_REPOSITORIES,
-          variables: { query, first, last, before, after },
-        },
-      ]}
+      mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
+      
     >
       {(addOrRemoveStar) => <StarStatus addOrRemoveStar={addOrRemoveStar} />}
     </Mutation>
